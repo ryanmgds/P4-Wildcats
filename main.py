050334b526fba860973097f2a514558ad9b1254b
Bryce Modugno <bmodugno1000@gmail.com>
2020-12-17 22:43:54 -0800

    Templating

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 @app.route('/main')
 def main():
-    return render_template("main.html", food=food)
+    return render_template("main.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port='3000', host='0.0.0.0')
