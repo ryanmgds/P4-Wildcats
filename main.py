ee9fd53e7fbdc26f410d78b536ad61a048f35e50
Bryce Modugno <bmodugno1000@gmail.com>
2021-01-20 15:20:53 -0800

    formatting

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 
 import smtplib
 
+# from bs4 BeautifulSoup import
+
 app = Flask(__name__)
 
 
@@ -9,14 +11,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+# @app.route('/scrubbing')
+# def scrubbing():
+#     url = 'https://covid.cdc.gov/covid-data-tracker/#cases_totalcases'
+#     r = requests.get(url)
+#     soup = BeautifulSoup(r.text, 'html.parser')
+#     print(soup)
+#     return render_template("home.html")
+
 @app.route('/email', methods = ['POST'])
 def email():
     email = request.form['email']
-    email_text = 'Subject: {}\n\n{}'.format("California Total Cases", 'California Total Cases 2,816,969')
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
     server.login('wildcatsp4@gmail.com', 'MrMadman33')
-    server.sendmail('wildcatsp4@gmail.com', email, email_text)
+    server.sendmail('wildcatsp4@gmail.com', email, "California Total Cases = 29324890u123")
     server.close()
     print ("email sent to:", email)
     return render_template("home.html")
