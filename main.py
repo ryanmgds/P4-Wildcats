928a55df3560ff46a921767713d19b149b44b1f1
Bryce Modugno <bmodugno1000@gmail.com>
2021-01-20 15:09:15 -0800

    Completed Email Sending

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
+import smtplib
 
 app = Flask(__name__)
 
@@ -7,9 +9,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route('/email', methods = ['POST'])
+def email():
+    email = request.form['email']
+    email_text = 'Subject: {}\n\n{}'.format("California Total Cases", 'California Total Cases 2,816,969')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login('wildcatsp4@gmail.com', 'MrMadman33')
+    server.sendmail('wildcatsp4@gmail.com', email, email_text)
+    server.close()
+    print ("email sent to:", email)
+    return render_template("home.html")
+
 @app.route('/main')
 def main():
-    return render_template("main.html")
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
